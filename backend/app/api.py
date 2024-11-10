f8a15b73c88fd9cf512bd6ba50787b17257a3f84
Aleksi Kesälä <alkesala@gmail.com>
2024-11-10 16:16:29 +0200

    feat: api-endpoints done.

--- backend/app/api.py
+++ backend/app/api.py
@@ -1,22 +1,96 @@
-from fastapi import APIRouter, Depends, HTTPException
+from starlette import status
+from fastapi import APIRouter, Depends, HTTPException, Response
 from sqlalchemy.orm import Session
-from typing import List
-from . import crud, models, schemas
+from . import models, schemas
 from .database import get_db
+from typing import Optional
 
 
 
 router = APIRouter()
+"""API ENDPOINTS"""
 
-"""API ENDPOINT TO GET ALL RIDES"""
-@router.post("/rides/", response_model=List[schemas.Ride])
-def get_rides(db: Session = Depends(get_db)):
-    rides = crud.get_rides(db)
-    return rides
+@router.get(
+    "/rides",
+    response_model=schemas.GetRidesSchema,
+    status_code=status.HTTP_200_OK
+)
+def get_rides(limit: Optional[int] = None,
+    db: Session = Depends(get_db)):
+    query = db.query(models.Ride)
+    if limit:
+        query = query.limit(limit)
+    rides = query.all()
+    return {"rides": rides}
 
-"""
-@router.get()
-@router.post()
-@router.put()
-@router.delete()
-"""
+
+@router.post(
+    "/rides",
+    status_code=status.HTTP_201_CREATED,
+    response_model=schemas.Ride
+)
+def create_ride(
+    payload: schemas.RideCreate,
+    db: Session = Depends(get_db)
+):
+    db_ride = models.Ride(**payload.model_dump())
+    db.add(db_ride)
+    db.commit()
+    db.refresh(db_ride)
+    return db_ride
+
+@router.get(
+    "/rides/{ride_id}",
+    response_model=schemas.Ride,
+    status_code=status.HTTP_200_OK
+)
+
+def get_ride(ride_id: int, db: Session = Depends(get_db)):
+    ride = db.query(models.Ride).filter(models.Ride.id == ride_id).first()
+    if not ride:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Ride with ID {ride_id} not found")
+    return ride
+
+@router.put(
+    "/rides/{ride_id}",
+    response_model=schemas.Ride,
+    status_code=status.HTTP_200_OK
+)
+
+def update_ride(
+    ride_id: int,
+    ride_details: schemas.RideUpdate,
+    db: Session = Depends(get_db)
+):
+    ride = db.query(models.Ride).filter(models.Ride.id == ride_id).first()
+    if not ride:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Ride with ID {ride_id} not found"
+        )
+        
+    for key, value in ride_details.model_dump(exclude_unset=True).items():
+        setattr(ride, key, value)
+    db.commit()
+    db.refresh(ride)
+    return ride
+
+@router.delete(
+    "/rides/{ride_id}",
+    status_code=status.HTTP_204_NO_CONTENT,
+    response_class=Response
+)
+
+def delete_ride(ride_id: int, db: Session = Depends(get_db)):
+    ride = db.query(models.Ride).filter(models.Ride.id == ride_id).first()
+    if not ride:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Ride with ID {ride_id} not found"
+        )
+    db.delete(ride)
+    db.commit()
+    return
+        
+    
+    
