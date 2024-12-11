7b9d97ddf3b02e51c985ffe77cf211d2ac23945d
Aleksi Kesälä <alkesala@gmail.com>
2024-12-11 23:00:49 +0200

    heroku deploy

--- app/main.py
+++ app/main.py
@@ -1,14 +1,22 @@
 # backend/app/main.py
 from fastapi import FastAPI
-# from fastapi.middleware.cors import CORSMiddleware FOR DEBUGGING ONLY
 from . import models
 from .database import engine
 from app.auth_router import auth_router
 from .api import router as api_router
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
+from pathlib import Path
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="CampusRide API", debug=False) 
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+
+@app.get("/", response_class=HTMLResponse)
+def frontend():
+    with open(Path("app/templates/index.html")) as file:
+        return HTMLResponse(content=file.read())
 
 
 app.include_router(api_router, prefix="/api", tags=["Rides and Bookings"])
