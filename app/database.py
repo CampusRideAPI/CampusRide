e9f0cc2a4d68726e4baad91c79d9c890ee5ac7c4
Aleksi Kesälä <alkesala@gmail.com>
2024-12-11 23:07:28 +0200

    heroku deploy. MIKSI PYTHONILLA

--- app/database.py
+++ app/database.py
@@ -5,14 +5,14 @@ import os
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
-engine = create_engine(
-    DATABASE_URL, 
-    connect_args={"sslmode": "require"} if "postgres" in DATABASE_URL else {}
-)
+engine = create_engine(DATABASE_URL, connect_args={"sslmode": "require"} if "postgresql" in DATABASE_URL else {})
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
+
 
 def get_db():
     db = SessionLocal()
